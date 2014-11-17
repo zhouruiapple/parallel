4185a31a94424915043da1625ca2055649384380
Victor Rodriguez <victor.manuel.rodriguez.bahena@intel.com>
2014-11-17 12:11:29 -0600

    Add CUDA examples
    Signed-off-by: Victor Rodriguez <victor.manuel.rodriguez.bahena@intel.com>

---
+++ cuda/addVector_shared.cu
@@ -0,0 +1,67 @@
+#include<stdio.h> 
+#include <time.h>
+#define N 256 
+
+//
+// Based on:
+// http://www.nvidia.com/docs/IO/116711/sc11-cuda-c-basics.pdf
+//
+
+__global__ void addvec(int*a, int*b, int*c) { 
+
+    __shared__ int s[N];
+        
+    if(threadIdx.x<N) {
+        s[threadIdx.x]=a[threadIdx.x]+b[threadIdx.x]; 
+    }
+    __syncthreads(); 
+    c[threadIdx.x]=s[threadIdx.x]; 
+} 
+
+int main(void){
+
+    clock_t begin, end;
+    double time_spent;
+    begin = clock();
+
+    int a[N], b[N], c[N];       // host variables
+    int *dev_a, *dev_b, *dev_c; // device variables
+
+    //allocate memory on the GPU 
+    cudaMalloc((void**)&dev_a,N * sizeof(int)); 
+    cudaMalloc((void**)&dev_b,N * sizeof(int)); 
+    cudaMalloc((void**)&dev_c,N * sizeof(int)); 
+
+    //Fill arrays with data
+    for(int i=0; i<N;i++){ 
+        a[i]=i; 
+        b[i]=i+1; 
+    } 
+
+    //copy arrays to GPU
+    cudaMemcpy(dev_a,a,N * sizeof(int),cudaMemcpyHostToDevice); 
+    cudaMemcpy(dev_b,b,N * sizeof(int),cudaMemcpyHostToDevice); 
+
+    //START KERNEL
+    addvec<<<N,1>>>(dev_a,dev_b,dev_c); 
+
+    //Copy result array to host 
+    cudaMemcpy(c,dev_c,N * sizeof(int),cudaMemcpyDeviceToHost); 
+    
+    //Print the result ( comment if you want to mesure time ) 
+    /*for(int i=0; i<N; i++){ 
+        printf("%d+%d=%d\n",a[i],b[i],c[i]); 
+    } */
+
+    // Free device memory
+    cudaFree(dev_a); 
+    cudaFree(dev_b); 
+    cudaFree(dev_c); 
+
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+    printf(" Time : %f\n", time_spent );
+
+    return 0; 
+} 
